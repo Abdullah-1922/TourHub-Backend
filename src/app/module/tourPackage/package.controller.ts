7d55215aaf1c5f369750b9fb2031e7623894eb21
Abdullah-1922 <kafikafi1922@gmail.com>
2024-09-20 15:45:16 +0600

    auth and builder query middleware added

--- src/app/module/tourPackage/package.controller.ts
+++ src/app/module/tourPackage/package.controller.ts
@@ -3,17 +3,25 @@ import catchAsync from "../../utils/catchAsync";
 import { PackageServices } from "./package.service";
 import sendResponse from "../../utils/sendResponse";
 
-const createPackage = catchAsync(
-    async(req,res)=>{
-        const result = await PackageServices.createPackage(req.body);
-        sendResponse(res,{
-            statusCode: httpStatus.OK,
-            success: true,
-            message: "Package is created successfully",
-            data: result,
-        });
-    }
-)
+const createPackage = catchAsync(async (req, res) => {
+  const result = await PackageServices.createPackage(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Package is created successfully",
+    data: result,
+  });
+});
+const getAllPackage = catchAsync(async (req, res) => {
+  const result = await PackageServices.getAllPackage(req?.query);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Package retrieved successfully",
+    data: result,
+  });
+});
 export const PackageControllers = {
-    createPackage,
+  createPackage,
+  getAllPackage
 };
